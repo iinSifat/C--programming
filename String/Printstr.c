8900c7941ace806879f46fdc583167fcb0d73cd2
iinSifat <iffatibnenashirsifat@gmail.com>
2024-03-18 01:38:19 +0600

    String

---
+++ String/Printstr.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    
+    char A[10];
+    printf("Enter your input:");
+    gets(A);
+    printf("Output:");
+    puts(A);
+
+    
+    
+return 0;
+
+}
